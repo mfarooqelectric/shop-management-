ad1175570653bd78f8b37cc75d2f6842bd3fe461
mfarooqelectric <mfarooqelectric29@gmail.com>
2026-09-02 07:47:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -273,7 +273,8 @@ elif choice == "Profit & Loss Dashboard":
 elif choice == "Inventory":
     st.subheader("📦 Main Stock (Products)")
     stock_df = pd.read_sql("SELECT id, product_name, category, quantity, unit_price, cost_price FROM products", conn)
-        st.dataframe(df, use_container_width=True)  
+def display_data()     
+ st.dataframe(df, use_container_width=True)
 git add requirements.txt
 git commit -m "Add reportlab to requirements.txt"
 git push    git add app.py
