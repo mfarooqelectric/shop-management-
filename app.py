4949ff08327566abb80d1c04d29301acd45e399a
mfarooqelectric <mfarooqelectric29@gmail.com>
2026-09-08 04:45:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def save_sheet_data(worksheet_name, df):
     ws.update([cleaned_df.columns.values.tolist()] + cleaned_df.values.tolist())
         return df
     except Exception:
-        return pd.DataFrame(columns=default_cols)
+    return pd.DataFrame(columns=default_cols)
 
 def save_sheet_data(worksheet_name, df):
     """Google Sheet me data save karne ke liye helper function"""
