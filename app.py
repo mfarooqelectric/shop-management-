8fae44a5ed8db00d1e70539ef31bd1d8bda93359
mfarooqelectric <mfarooqelectric29@gmail.com>
2026-09-11 18:35:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -417,7 +417,7 @@ elif choice == "Purchase Return":
     else:
         st.warning("Pehle Supplier Management se purchases add karein.")
                 # Save return in "purchase_returns"
-                gc = get_gspread_client()
+                    gc = get_gspread_client()
                 try:
                     sh = gc.open(SHEET_NAME)
                 except Exception as e:
