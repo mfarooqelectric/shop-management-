fd7d0661447cf10b08255ddd5e5264118f0f1631
mfarooqelectric <mfarooqelectric29@gmail.com>
2026-09-12 17:08:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -321,7 +321,7 @@ new_supp_entry = pd.DataFrame([{
     "balance": running_balance,  # ← RUNNING BALANCE
     "date": today_date,
 }])
-                gc = get_gspread_client()
+               gc = get_gspread_client()
                 sh = gc.open(SHEET_NAME)
                 worksheet_supp = sh.worksheet("supplier_ledger")
                 
