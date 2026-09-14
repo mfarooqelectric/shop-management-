3f0e8e57f7c83f50741603befa3253eeacc7d69a
mfarooqelectric <mfarooqelectric29@gmail.com>
2026-09-14 16:41:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -319,7 +319,8 @@ else:
     "paid_amount": paid_amt, 
     "balance": tot_amt - paid_amt,
     "date": today_date
-     }])           
+}])
+    
      gc = get_gspread_client()
      sh = gc.open(SHEET_NAME)
      worksheet_supp = sh.worksheet("supplier_ledger")
