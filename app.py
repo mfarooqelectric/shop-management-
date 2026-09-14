1d5a90402c3fbaa032364cefc8fd71af99fb4540
mfarooqelectric <mfarooqelectric29@gmail.com>
2026-09-14 17:40:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -324,12 +324,16 @@ gc = get_gspread_client()
 sh = gc.open(SHEET_NAME)
 worksheet_supp = sh.worksheet("supplier_ledger")
                 
-   if len(supp_ledger_df) == 0:
-      worksheet_supp.update([new_supp_entry.columns.tolist()] + new_supp_entry.values.tolist())
-   else:
-      worksheet_supp.append_rows(new_supp_entry.values.tolist())
+  gc = get_gspread_client()
+sh = gc.open(SHEET_NAME)
+worksheet_supp = sh.worksheet("supplier_ledger")
 
-    st.success(f"Stock Added - Company: {final_company}, Godown: {selected_godown}!")
+if len(supp_ledger_df) == 0:
+    worksheet_supp.update([new_supp_entry.columns.tolist()] + new_supp_entry.values.tolist())
+else:
+    worksheet_supp.append_rows(new_supp_entry.values.tolist())
+
+st.success(f"Stock Added - Company: {final_company}, Godown: {selected_godown}!")
 
 # --- PROFIT & LOSS DASHBOARD ---
 elif choice == "Profit & Loss Dashboard":
